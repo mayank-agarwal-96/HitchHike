696639650604353320f717066a5a5287a1389795
Mayank Agrawal <agarwalm214@gmail.com>
2016-10-07 02:47:33 +0530

    change database connectivity

--- welcome.py
+++ welcome.py
@@ -19,6 +19,7 @@ from flask import Flask, session, redirect, render_template, g, url_for, request
 from datetime import datetime
 from couchdb.mapping import Document, TextField, DateTimeField, ListField, FloatField, IntegerField, BooleanField
 from User.controller import user
+from config import cloudant_data
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -32,15 +33,19 @@ app.register_blueprint(user,url_prefix="/user")
 def Welcome():
     return app.send_static_file('index.html')
 
-@app.route('/home')
-def after_login():
-    if g.user:
-        return render_template('welcome.html')
-
-    return redirect(url_for('login'))
+def get_db():
+    if not hasattr(g, 'db'):
+        server = couchdb.Server("https://"+cloudant_data['user']+':'+cloudant_data['password']+'@'+cloudant_data['host']+':'+cloudant_data['port'])
+        with app.app_context():
+            try:
+                db = server.create('hitchhike')
+            except:
+                db = server['hitchhike']
+    return db
 
 @app.before_request
 def before_request():
+    g.db = get_db()
     g.user = None
     if 'user' in session:
         g.user = session['user']
