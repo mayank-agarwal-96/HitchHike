cd76a301762dcab3e8d4c90c6d64f9c071b2a2dc
Mayank Agrawal <agarwalm214@gmail.com>
2016-11-09 01:25:16 +0530

    basic fix

--- HitchHike/database.py
+++ HitchHike/database.py
@@ -5,13 +5,13 @@ from config import cloudant_data
 
 class DataBase:
     _url = "https://"+cloudant_data['user']+':'+cloudant_data['password']+'@'+cloudant_data['host']+':'+cloudant_data['port']
-    _name = "hitchhike"
+    _dbname = "hitchhike"
     _db = None
     _server = None
 
     @classmethod
     def db(cls):
         cls._server = couchdb.Server(cls._url)
-        cls._db = cls._server(cls._name)
+        cls._db = cls._server[cls._dbname]
 
         return cls._db
